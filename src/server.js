fca1973749d4b2ee2e1bafbfae84b9174c21bd64
Luis Monte <luismonte.l560@gmail.com>
2022-05-31 09:10:03 -0300

    feat: add endpoints POST /login & POST /user

--- src/server.js
+++ src/server.js
@@ -1,8 +1,27 @@
+const loginValidation = require('./middlewares/loginMiddleware');
+const loginController = require('./controllers/loginController');
+const userController = require('./controllers/userController');
+const errorMiddlewares = require('./middlewares/errorMiddleware');
+const {
+  displayNameValidation,
+  emailValidation,
+  passwordValidation,
+} = require('./middlewares/userMiddlewares');
+
 require('dotenv').config();
 const app = require('./api');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
+
+app.post('/login', loginValidation, loginController);
+app.post('/user',
+  displayNameValidation,
+  emailValidation,
+  passwordValidation,
+  userController);
+
+app.use(errorMiddlewares);
 
 // não remova esse endpoint
 app.get('/', (_request, response) => {
