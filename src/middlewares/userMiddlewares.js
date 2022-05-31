7f8a4c01cbc43d710c3992a7a5dd3a557213f781
Luis Monte <luismonte.l560@gmail.com>
2022-05-31 09:09:14 -0300

    feat: add userMiddlewares

---
+++ src/middlewares/userMiddlewares.js
@@ -0,0 +1,39 @@
+const displayNameValidation = (req, res, next) => {
+  const { displayName } = req.body;
+
+  if (displayName.length < 8) {
+    return res
+      .status(400)
+      .json({ message: '"displayName" length must be at least 8 characters long' });
+  }
+
+  next();
+};
+
+const emailValidation = (req, res, next) => {
+  const { email } = req.body;
+
+  if (!(email.includes('@') && email.includes('.com'))) {
+    return res.status(400).json({ message: '"email" must be a valid email' });
+  }
+
+  next();
+};
+
+const passwordValidation = (req, res, next) => {
+  const { password } = req.body;
+
+  if (password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: '"password" length must be at least 6 characters long' });
+  }
+
+  next();
+};
+
+module.exports = {
+  displayNameValidation,
+  emailValidation,
+  passwordValidation,
+};
