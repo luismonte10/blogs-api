183c8dcd1a688bf83675bd6a97a0c20e8a92381f
Luis Monte <luismonte.l560@gmail.com>
2022-06-02 00:23:05 -0300

    fix: gambiarra para corrigir o retorno ao validar token

--- src/middlewares/authToken.js
+++ src/middlewares/authToken.js
@@ -2,19 +2,23 @@ const jwt = require('jsonwebtoken');
 
 const authToken = (req, res, next) => {
   try {
-    const token = req.headers.authorization;
+      const token = req.headers.authorization;
+    
+      if (!token) return res.status(401).json({ message: 'Token not found' });
 
-    if (!token) return res.status(401).json({ message: 'Token not found' });
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    if (!decoded.data) return res.status(401).json({ message: 'Expired or invalid token' });
-
-    next();
-  } catch (error) {
-    console.log('authToken: ', error.message);
-    next(error);
-  }
+      console.log(decoded);
+  
+      // if (!decoded.data) {
+      //   console.log('Entrou aqui?');
+      //   return res.status(401).json({ message: 'Expired or invalid token' });
+      // }
+  
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: 'Expired or invalid token' });
+    }
 };
 
 module.exports = authToken;
