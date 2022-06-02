4409ce274d4ed1e6128d3cf784e2ad05a01eab77
Luis Monte <luismonte.l560@gmail.com>
2022-06-02 00:38:04 -0300

    feat: add getUserById userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -29,7 +29,21 @@ const getUsers = async (req, res, next) => {
   }
 };
 
+const getUserById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    
+    const userById = await userService.getUserById(id);
+
+    return res.status(200).json(userById);
+  } catch (error) {
+    console.log('getUserById: ', error.message);
+    next(error);
+  }
+};
+
 module.exports = {
   createUser,
   getUsers,
+  getUserById,
 };
